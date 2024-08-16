4050526fcc43543904ceba33c574a4e4157aac2c
mmannings <mark.vanin@tib.eu>
2024-08-16 09:33:31 +0200

    Style fixes with some templates extending the form, expanded panels hiding submit button and template text overflowing card content and becoming not visible.

--- angular/src/app/reporting/list/list.component.ts
+++ angular/src/app/reporting/list/list.component.ts
@@ -1,18 +1,19 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
-import {first} from 'rxjs';
-import {Report} from '../models/report';
-import {ReportService} from '../services/report.service';
-import {UpdReport} from "../models/updReport";
-import {ConfirmDialogComponent} from "../common/confirm-dialog/confirm-dialog.component";
-import {MatDialog} from '@angular/material/dialog';
-import {MatSnackBar} from "@angular/material/snack-bar";
-import * as JSZip from "jszip";
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { first } from 'rxjs';
+import { Report } from '../models/report';
+import { ReportService } from '../services/report.service';
+import { UpdReport } from '../models/updReport';
+import { ConfirmDialogComponent } from '../common/confirm-dialog/confirm-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import * as JSZip from 'jszip';
 import mime from 'mime';
-import {SUPPPORTED_CONTENT_TYPES} from "../models/SupportedContentTypes";
-import {Router} from "@angular/router";
-import {FileUtils} from "../utils/FileUtils";
+import { SUPPPORTED_CONTENT_TYPES } from '../models/SupportedContentTypes';
+import { Router } from '@angular/router';
+import { FileUtils } from '../utils/FileUtils';
+import { EditReportComponent } from '../edit-report/edit-report.component';
 
 interface ApiResponse {
   reports: UpdReport[];
@@ -32,10 +33,12 @@ export class ListComponent implements OnInit, AfterViewInit {
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(private reportService: ReportService,
-              private dialog: MatDialog,
-              private snackBar: MatSnackBar,
-              private router: Router) {}
+  constructor(
+    private reportService: ReportService,
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
     this.getData();
@@ -45,7 +48,7 @@ export class ListComponent implements OnInit, AfterViewInit {
     this.reportService.getAll().subscribe((response: ApiResponse) => {
       this.dataSource.data = response.reports;
       console.log(this.dataSource.data);
-    })
+    });
   }
 
   ngAfterViewInit(): void {
@@ -57,7 +60,9 @@ export class ListComponent implements OnInit, AfterViewInit {
   }
 
   startReportWithoutConstruct() {
-    this.router.navigate(['/add'], { queryParams: { includeConstruct: false } });
+    this.router.navigate(['/add'], {
+      queryParams: { includeConstruct: false },
+    });
   }
 
   deleteReport(resourceId: string) {
@@ -66,7 +71,7 @@ export class ListComponent implements OnInit, AfterViewInit {
       .pipe(first())
       .subscribe(() => {
         this.dataSource.data = this.dataSource.data.filter(
-          (r) => r.resource_id.value !== resourceId
+          (r) => r.resource_id.value !== resourceId,
         );
       });
   }
@@ -75,11 +80,11 @@ export class ListComponent implements OnInit, AfterViewInit {
     const dialogRef = this.dialog.open(ConfirmDialogComponent, {
       data: {
         title: 'Confirm Deletion',
-        message: `Are you sure you want to delete report?`
-      }
+        message: `Are you sure you want to delete report?`,
+      },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       if (result) {
         this.deleteReport(resourceId);
       }
@@ -97,21 +102,25 @@ export class ListComponent implements OnInit, AfterViewInit {
         } else {
           console.error('File reading failed. reader.result is null.');
         }
-      }
+      };
     }
   }
 
   importReport(configurationGraph: string): void {
-    this.reportService.import(configurationGraph)
+    this.reportService
+      .import(configurationGraph)
       .pipe(first())
       .subscribe({
         next: () => {
           this.getData();
-          this.showSnackBar('Success: Report configuration imported successfully!', 'success');
+          this.showSnackBar(
+            'Success: Report configuration imported successfully!',
+            'success',
+          );
         },
         error: () => {
           this.showSnackBar('Error: Config file is not correct.', 'error');
-        }
+        },
       });
   }
 
@@ -121,24 +130,51 @@ export class ListComponent implements OnInit, AfterViewInit {
     });
   }
 
+  editReport(resourceId: string) {
+    const dialogRef = this.dialog.open(EditReportComponent, {
+      height: '70vh',
+      width: '50vh',
+      data: {
+        uri: resourceId,
+      },
+    });
+  }
+
   async downloadReport(resourceId: string) {
-    this.reportService.execute(resourceId).subscribe(async (response) => {
-      const base64Data = response.report;
-      const blob = FileUtils.base64ToBlob(base64Data);
-      const contentType = await FileUtils.getContentTypeFromZip(blob);
-      this.triggerDownload(blob, `report.${contentType}`)
+    this.reportService.execute(resourceId).subscribe({
+      next: async (response) => {
+        const base64Data = response.report;
+        const blob = FileUtils.base64ToBlob(base64Data);
+        const contentType = await FileUtils.getContentTypeFromZip(blob);
+        this.triggerDownload(blob, `report.${contentType}`);
+        this.showSnackBar(
+          'Success: Report downloaded successfully!',
+          'success',
+        );
+      },
+      error: () => {
+        this.showSnackBar('Error: Failed to download the report.', 'error');
+      },
     });
   }
 
   downloadConfig(resourceId: string) {
-    this.reportService.export(resourceId).subscribe((response) => {
-      const configGraph = response.report_generator_configuration_graph;
-      const contentType = mime.getType(SUPPPORTED_CONTENT_TYPES.N3) as string;
-      const blob = new Blob([configGraph], {type: contentType});
-      this.triggerDownload(blob, `config.${SUPPPORTED_CONTENT_TYPES.N3}`)
-    })
+    this.reportService.export(resourceId).subscribe({
+      next: (response) => {
+        const configGraph = response.report_generator_configuration_graph;
+        const contentType = mime.getType(SUPPPORTED_CONTENT_TYPES.N3) as string;
+        const blob = new Blob([configGraph], { type: contentType });
+        this.triggerDownload(blob, `config.${SUPPPORTED_CONTENT_TYPES.N3}`);
+        this.showSnackBar(
+          'Success: Config downloaded successfully!',
+          'success',
+        );
+      },
+      error: () => {
+        this.showSnackBar('Error: Failed to download the config.', 'error');
+      },
+    });
   }
-
 
   triggerDownload(blob: Blob, filename: string): void {
     const url = window.URL.createObjectURL(blob);
